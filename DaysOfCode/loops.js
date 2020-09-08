7dcf4c32136472320062c358a8539f6516770226
StevBL <stevenbraloz007@gmail.com>
2020-09-08 09:12:17 -0600

    fix: loops.js corrected

--- DaysOfCode/loops.js
+++ DaysOfCode/loops.js
@@ -1,25 +1,23 @@
-'use strict';
-
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 
 let inputString = '';
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
+process.stdin.on('data', (inputStdin) => {
     inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
+process.stdin.on('end', () => {
     inputString = inputString.replace(/\s*$/, '')
         .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
+        .map((str) => str.replace(/\s*$/, ''));
 
     main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+    return inputString[currentLine += 1];
 }
 
 function main() {
@@ -28,11 +26,10 @@ function main() {
 
     for (let i = 1; i <= 10; i += 1) {
         if (i < 10) {
-            result += `${n} x ${i} = ${n*i}\n`;
+            result += `${n} x ${i} = ${n * i}\n`;
         } else {
-            result += `${n} x ${i} = ${n*i}`;
+            result += `${n} x ${i} = ${n * i}`;
         }
-
     }
 
     console.log(result);
