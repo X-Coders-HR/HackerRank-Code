c38ff647d20404cd98a1dc50992513822520fcb8
StevBL <stevenbraloz007@gmail.com>
2020-09-08 08:52:27 -0600

    feat: add Day 5: loops.js

---
+++ DaysOfCode/loops.js
@@ -0,0 +1,39 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+    let result = '';
+
+    for (let i = 1; i <= 10; i += 1) {
+        if (i < 10) {
+            result += `${n} x ${i} = ${n*i}\n`;
+        } else {
+            result += `${n} x ${i} = ${n*i}`;
+        }
+
+    }
+
+    console.log(result);
+}
